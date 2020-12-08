1bb739984ac1a3708071c2420d4050dbc38cfe7f
jordan hurst <jordanhurst@gmail.com>
2020-12-08 10:43:52 -0600

    almost 100%

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between 5 and 15 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Username must be between 6 and 20 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [Compare("Password", ErrorMessage = "Passwords must match.")]
+        public string VerifyPassword { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
